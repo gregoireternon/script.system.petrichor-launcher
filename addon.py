2bcd0499d3651d9924fa747688c1fedbc8d19548
Gregoire Ternon <gregoire.ternon@gmail.com>
2018-05-17 22:05:00 +0200

    Applying source modifications

--- addon.py
+++ addon.py
@@ -4,8 +4,9 @@ import xbmcgui
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
+line1 = "PETRICHOR"
+line2 = "Use this add on to launch petrichor services"
 line3 = "Using Python"
  
 xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+
